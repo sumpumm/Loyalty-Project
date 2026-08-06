2ef887897402992dfa9649664aea9521be825d69
sumpumm <samarpan69.piya@gmail.com>
2026-08-06 22:56:06 +0545

    wallet model created

---
+++ models/wallet.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const walletSchema = new mongoose.Schema({
+    user:{
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+        required: true,
+    },
+    business:{
+        type: Schema.Types.ObjectId,
+        ref: 'business',
+        required: true,
+    },
+    stampCount: { type: Number, default: 0, min: 0 },
+    totalStamps: { type: Number, default: 0 },
+    joinedAt: { type: Date, default: Date.now },
+},
+{ timestamps: true });
+
+const WALLET = mongoose.model('wallet',walletSchema);
+
+module.exports = WALLET;
