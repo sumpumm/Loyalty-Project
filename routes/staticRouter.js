83e618b10b2de5a47dab7fe4516f944ca4790fc5
sumpumm <samarpan69.piya@gmail.com>
2026-08-06 12:06:11 +0545

    simple login and signup pages were created.

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -6,4 +6,12 @@ router.get('/',(req,res)=>{
     return res.render('home');
 });
 
+router.get('/login',(req,res)=>{
+    return res.render('login');
+});
+
+router.get('/signup',(req,res)=>{
+    return res.render('signup');
+});
+
 module.exports = router;
